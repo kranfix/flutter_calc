6432dd06da111fbcb07040d640d741c76f5621b0
Frank Moreno <frankmoreno1993@gmail.com>
2018-11-17 20:23:40 -0500

    core: clear and backspace

--- calc_core/lib/calc.dart
+++ calc_core/lib/calc.dart
@@ -10,38 +10,72 @@ class Calculator {
   Number num1, num2;
   Op _op = Op.idle;
 
-  Calculator() {}
-
-  addNumber() {}
-
-  addPoint() {}
-
-  String operate() {
-    try {
-      switch (_op) {
-        case Op.idle:
-          return num1.literal;
-          break;
-        case Op.sum:
-          num1 += num2;
-          break;
-        case Op.sub:
-          num1 -= num2;
-          break;
-        case Op.prod:
-          num1 *= num2;
-          break;
-        case Op.div:
-          num1 /= num2;
-          break;
-      }
-    } catch (e) {}
-
-    num2.clear();
-    return "";
+  Calculator() {
+    num1 = Number();
+    num2 = Number();
   }
 
-  String get X {
+  addDigit(int digit) {
+    if (_op == Op.idle) {
+      num1.addDigit(digit);
+    } else {
+      num2.addDigit(digit);
+    }
+  }
+
+  addPoint() {
+    if (_op == Op.idle) {
+      num1.addPoint();
+    } else {
+      num2.addPoint();
+    }
+  }
+
+  setAdditionOperator() {
+    _op = Op.sum;
+  }
+
+  setSubtranctionOperator() {
+    _op = Op.sub;
+  }
+
+  setProductOperator() {
+    _op = Op.prod;
+  }
+
+  setDivisinOperator() {
+    _op = Op.div;
+  }
+
+  String operate() {
+    switch (_op) {
+      case Op.idle:
+        return num1.literal;
+        break;
+      case Op.sum:
+        num1 += num2;
+        break;
+      case Op.sub:
+        num1 -= num2;
+        break;
+      case Op.prod:
+        num1 *= num2;
+        break;
+      case Op.div:
+        try {
+          num1 /= num2;
+        } catch (e) {
+          return 'NaN';
+        }
+        break;
+    }
+
+    _op = Op.idle;
+    num2.clear();
+    return num1.literal;
+  }
+
+  String get text {
     String op;
     switch (_op) {
       case Op.idle:
@@ -59,6 +93,22 @@ class Calculator {
         op = "/";
         break;
     }
-    return num1.literal + op + num2.literal;
+    return num1.literal + op + (num2.isVoid ? '' : num2.literal);
+  }
+
+  backspace() {
+    if (_op == Op.idle) {
+      num1.del();
+    } else if (num2.isVoid) {
+      _op = Op.idle;
+    } else {
+      num2.del();
+    }
+  }
+
+  clear() {
+    _op = Op.idle;
+    num1.clear();
+    num2.clear();
   }
 }
